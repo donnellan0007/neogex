89eccbdadb2049a52e81643293e74ac2da94fad5
Seamus Donnellan <donnellan0007@gmail.com>
2021-12-18 02:37:07 +1100

    Basic parser - sort of

--- src/lexer.py
+++ src/lexer.py
@@ -10,7 +10,12 @@ TOKENS = (
     'TEXT',
     'MIN',
     'MAX',
-    'CHAR',    
+    'CHAR',
+    'and',
+    'or',
+    'not',
+    'includes',
+    'with',
 )
 
 HELPERS = (
@@ -27,7 +32,7 @@ def lex(string):
     for i in string:
         # If the character is a space, then we have reached the end of a token.
         token.append(i)
-        if i == ' ' or i == '':
+        if i == ' ':
             token = []
         token_ = ("").join(token)
         token_ = token_.strip()
@@ -35,14 +40,44 @@ def lex(string):
         check if token_ is a number, as numbers are tokens.
         ideally, this will be consolidated into a if statement
         """
+        # check if character after token is a double space
         if token_.isdigit():
             tokens.append(token_)
-            print("Token: " + ("").join(token))
+            # print("Token: " + ("").join(token))
         else:
             if token_ in TOKENS:
                 tokens.append(token_)
-                print("Token: " + ("").join(token))
+                # print("Token: " + ("").join(token))
 
     return tokens
 
-print(lex("TEXT with MIN 8 CHAR and MAX 10 CHAR"))
+# print(lex("TEXT with MIN 8 CHAR and MAX 20 CHAR and includes[1:] {'@', '#'}"))
+# print(lex("TEXT with MIN 8 CHAR and MAX 9 CHAR"))
+
+def parser(tokens):
+    """
+    Takes a list of tokens and returns a list of parsed tokens.
+    """
+    parsed = []
+    for i in tokens:
+        if i in TOKENS:
+            parsed.append(i)
+        elif i in HELPERS:
+            parsed.append(i)
+        else:
+            parsed.append(int(i))
+    return parsed
+
+def parsing(tokens):
+
+    counter = 0
+    elem_pos = {}
+
+    if tokens[0] == 'TEXT':
+        for i in tokens:
+            counter = counter + 1
+            elem_pos[counter] = i
+        print(elem_pos)
+
+
+print(parsing(lex("TEXT with MIN 8 CHAR and MAX 9 CHAR")))
